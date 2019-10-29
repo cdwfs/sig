1c0a657a5b71890569662e26bea6ea4c6c217f94
Cort <1944792+cdwfs@users.noreply.github.com>
2019-10-28 23:24:30 -0700

    More comments, more cleanup

--- sig2.c
+++ sig2.c
@@ -1,13 +1,10 @@
-/**
- * /dev/dsp doesn't exist any more! New plan:
- * 1) Output data directly to /dev/dsp instead of stdout
- * 2) Compile: clang -lm sig.c
- * 3) Use PulseAudio to redirect /dev/dsp traffic to an actual audio driver:
- *    padsp ./a.out
- */
+#if 0 // Original sig.c for reference
+#include <math.h> /*** ---- viznut --- http://www.hytti.uku.fi/~vheikkil/ */
+main(v,i,z,n,u,t){for(v=-1;;)for(n=pow(/* gcc -lm sig.c; a.out > /dev/dsp */
+1.06,"`cW`g[`cgcg[eYcb^bV^eW^be^bVecb^"[++v&31]+(v&64)/21),i=999;i;putchar(
+128+((8191&u)>i?0:i/8)-((8191&(z+=n))*i-->>16)))u+=v&1?t/2:(t=v&6?t:n/4);}
+#endif
 
-/*** ---- viznut --- http://www.hytti.uku.fi/~vheikkil/ */
-/* gcc -lm sig.c; a.out > /dev/dsp */
 #include <math.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -39,9 +36,8 @@ void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uin
 
 int main()
 {
+    // Initialize output buffer
     outputBuffer = (uint8_t*)malloc(OUTPUT_BUFFER_SIZE);
-
-    int z=0,u=0,t=0;
 
     ma_device audioDevice;
     ma_device_config audioDeviceConfig = ma_device_config_init(ma_device_type_playback);
@@ -63,6 +59,9 @@ int main()
       return -4;
     }
 
+    // set to zero while playing with the code to avoid a spew of errors.
+    // leave as one normally to validate that deobfuscation doesn't affect the output.
+    const int validateOutput = 1;
     unsigned char *refSamples = NULL;
     size_t refFileNbytes = 0;
     FILE *refFile = fopen("reference.bin", "rb");
@@ -76,6 +75,7 @@ int main()
         fclose(refFile);
     }
 
+    // The sequence of notes to play.
 #if 0
     // Original string as it appeared in viznut's code
     const char *str = "`cW`g[`cgcg[eYcb^bV^eW^be^bVecb^";
@@ -95,6 +95,7 @@ int main()
     };
 #endif
 
+    int z = 0, u = 0, t = 0;
     int noteIndex = -1;
     for(;;)
     {
@@ -110,18 +111,29 @@ int main()
         if (noteIndex & 64)
             note += 3;
         // 1.06 is (approximately) the ratio of the frequencies of
-        // successive notes in a 12-tone Western scale. We know that
+        // successive semitones in a 12-tone Western scale. We know that
         // each octave doubles the frequency. The increase between
         // each pair of notes is thus the 12th root of 2.0, or
         // 1.059463039(...).  This factor is VERY finicky; a
         // relatively small change will significantly affect the
         // tonality of the result. Try changing to 1.059, 1.058, etc.
-        double stepFreqRatio = 1.06; // viznut's approximation, rounded to two places for brevity.
+        double semitoneFreqRatio = 1.06; // viznut's approximation, rounded to two places for brevity.
         // double stepFreqRatio = pow(2.0, 1.0/12.0); // more precise value
 
-        // This gives a
-        int freqRatio = (int)pow(stepFreqRatio, note);
+        // Raising each note of the sequence by the semitoneFreqRatio gives the following sequence of values,
+        // which are the frequencies of each note to be played. Rounding the frequencies to integers does
+        // cause some quantization error, but it's close enough and every character counts.
+#if 0
+        int noteFreqs[] = {
+            268, 320, 159, 268, 404, 200, 268, 320,
+            404, 320, 404, 200, 359, 178, 320, 301,
+            239, 301, 150, 239, 359, 159, 239, 301,
+            359, 239, 301, 150, 359, 320, 301, 239,
+        };
+#endif
+        int freq = (int)pow(semitoneFreqRatio, note);
 
+        // Generate a value for each sample in this step of the sequence
         while(samplesPerStep != 0)
         {
             if (noteIndex & 1)
@@ -132,31 +144,47 @@ int main()
             else
             {
                 // Even notes take this path.
-                t = (noteIndex & 6) ? t : freqRatio/4;
+                t = (noteIndex & 6)
+                    ? t
+                    : freq / 4;
                 u += t;
             }
-            z += freqRatio;
-            unsigned char byte = (unsigned char)(128 +
-                ((8191&u)>samplesPerStep ? 0 : samplesPerStep/8) -
-                ((8191&z)*samplesPerStep >> 16));
+            z += freq;
+
+            // The original output code:
+            //   putchar(128 + ((8191 & u) > i ? 0 : i / 8) - ((8191 & (z += n)) * i-- >> 16))
+            // Regroup as:
+            //   putchar(128 + (              a           ) - (                b             )
+            int a = (8191 & u) > samplesPerStep
+                ? 0
+                : samplesPerStep / 8;
+            // Let's say you had a table of 8192 samples of a waveform. If you write 8192 samples/second
+            // from this table in a straight loop (for i=0; i<8192; i+=1), you would generate that waveform
+            // exactly as represented in the input. If you increment i by 2 instead of 1 each time through
+            // the loop, you'll generate the whole waveform in half the samples, resulting in double the
+            // frequency.
+            int b = ((8191 & z) * samplesPerStep) >> 16;
+            samplesPerStep -= 1;
+            unsigned char outputByte = (unsigned char)(128 + a - b);
+
             // Check against reference samples, if available
             static int sampleIndex = 0;
             if (refSamples &&
+                validateOutput &&
                 sampleIndex < refFileNbytes &&
-                byte != refSamples[sampleIndex])
+                outputByte != refSamples[sampleIndex])
             {
                 fprintf(stderr, "ERROR: sample mismatch at index %d (generated 0x%02X, expected 0x%02X)\n",
-                    sampleIndex, byte, refSamples[sampleIndex]);
+                    sampleIndex, outputByte, refSamples[sampleIndex]);
             }
             sampleIndex += 1;
-            if (sampleIndex == refFileNbytes)
+            if (validateOutput && sampleIndex == refFileNbytes)
                 fprintf(stderr, "All samples matched reference values until now\n");
+
             // Output
-            //fputc(byte, dsp);
             if (sampleCount < OUTPUT_BUFFER_SIZE) {
-              outputBuffer[sampleCount++] = byte;
+                outputBuffer[sampleCount++] = outputByte;
             }
-            samplesPerStep -= 1;
         }
     }
 
